6133abdcc21564e343152ea7cd8679248be4863b
bftawfik <bftawfik@gmail.com>
2021-07-16 10:26:18 +0200

    add weather data

--- src/mock/banner.js
+++ src/mock/banner.js
@@ -1,6 +1,6 @@
 const banner = {
   bgText: "cnn - 4 you",
-  title: "Hello weather news",
+  title: "Hello weather news.",
 };
 
 export default banner;
---
+++ src/mock/weather.js
@@ -0,0 +1,7 @@
+const weather = {
+  title: "Just type the city's name",
+  subtitle: "You must spell correctly",
+  submit: "Find",
+};
+
+export default weather;
