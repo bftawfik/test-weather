085a6b806f87d6cc22cb2b3f6f46ed921b1006a3
bftawfik <bftawfik@gmail.com>
2021-07-17 00:10:55 +0200

    link store to weather component

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,9 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useSelector, useDispatch } from "react-redux";
 
 import Navigation from "../Navigation/Navigation";
+import { getWeather } from "../../services/services";
+import { addCityName, addCityFullWeather } from "../../redux/city/citySlice";
 
 import missingData from "../../mock/weather";
 
@@ -11,8 +14,40 @@ import * as classes from "./Weather.module.scss";
 
 const labels = ["Home", "Weather"];
 
+const DataComp = ({ classes, stateCity }) => (
+  <div className={classes.data}>
+    <h2>{stateCity.name}</h2>
+    {stateCity.weather.icon && (
+      <img
+        src={`http://openweathermap.org/img/wn/${stateCity.weather.icon}@2x.png`}
+        alt={stateCity.weather.description}
+      />
+    )}
+    <h3>{stateCity.weather.title}</h3>
+    <p className={classes.temp}>
+      {stateCity.temp.value}
+      <sup>°</sup>
+    </p>
+    <p className={classes.tempRang}>
+      {stateCity.temp.max}
+      <sup>°</sup> {stateCity.temp.min}
+      <sup>°</sup>
+    </p>
+    <p className={classes.state}>{stateCity.weather.description}</p>
+  </div>
+);
+
 const Weather = () => {
   const slider = useRef(null);
+
+  const [inputValue, setInputValue] = useState("");
+
+  const dispatch = useDispatch();
+  const { userCity: stateUserCity, inputCity: stateInputCity } = useSelector(
+    (state) => state.city
+  );
+  const { name: stateInputCityName } = stateInputCity;
+  console.log(stateInputCity);
 
   const sliderSettingsBuilder = () => ({
     swipe: false,
@@ -23,7 +58,7 @@ const Weather = () => {
     arrows: true,
     className: classes.slider,
     accessibility: true,
-    focusOnSelect: true,
+    focusOnSelect: false,
     lazyLoad: "ondemand",
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -37,8 +72,55 @@ const Weather = () => {
   });
 
   const goTo = (id) => {
-    slider.current.slickGoTo(id);
+    console.log(slider.current.slickGoTo);
+    slider?.current?.slickGoTo(id);
   };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(addCityName({ type: "input", value: inputValue }));
+  };
+
+  const fetchLocalWeather = async (
+    dispatch,
+    addCityFullWeather,
+    stateInputCityName
+  ) => {
+    try {
+      const response = await getWeather(stateInputCityName);
+      const data = response.data;
+      const weather = data.weather.length > 0 ? data.weather[0] : undefined;
+      dispatch(
+        addCityFullWeather({
+          type: "input",
+          value: {
+            temp: {
+              value: data.main.temp,
+              min: data.main.temp_min,
+              max: data.main.temp_max,
+            },
+            weather:
+              weather === undefined
+                ? weather
+                : {
+                    title: weather.main,
+                    icon: weather.icon,
+                    description: weather.description,
+                  },
+          },
+        })
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (stateInputCityName) {
+      fetchLocalWeather(dispatch, addCityFullWeather, stateInputCityName);
+    }
+  }, [dispatch, stateInputCityName]);
+
   return (
     <div className={classes.Weather}>
       <Navigation
@@ -53,28 +135,34 @@ const Weather = () => {
         <section className={classes.slide}>
           <div className={classes.contents}>
             <h2 className={classes.title}>Home</h2>
+            {<DataComp classes={classes} stateCity={stateUserCity} />}
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                goTo(1);
+              }}
+            >
+              Choose a City !
+            </button>
           </div>
         </section>
         <section className={classes.slide}>
           <div className={classes.contents}>
             <h2 className={classes.title}>{missingData.title}</h2>
             <h3 className={classes.subtitle}>{missingData.subtitle}</h3>
-            <form className={classes.form}>
-              <input type="text" />
+            <form className={classes.form} onSubmit={submitHandler}>
+              <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => {
+                  setInputValue(e.target.value);
+                }}
+              />
               <button type="submit">{missingData.submit}</button>
             </form>
-            <div className={classes.data}>
-              <h2>Poland, US</h2>
-              <i src="" />
-              <h3>Clear</h3>
-              <p className={classes.temp}>
-                -4.76<sup>°</sup>
-              </p>
-              <p className={classes.tempRang}>
-                -5.35<sup>°</sup> -3.29<sup>°</sup>
-              </p>
-              <p className={classes.state}>Clear sky</p>
-            </div>
+            {stateInputCity.name !== undefined && (
+              <DataComp classes={classes} stateCity={stateInputCity} />
+            )}
           </div>
         </section>
       </Slider>
