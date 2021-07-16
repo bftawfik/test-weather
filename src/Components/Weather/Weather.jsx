cf66e760ab2722da5ee1303f167b5e62d714276a
bftawfik <bftawfik@gmail.com>
2021-07-16 11:55:51 +0200

    add react-slick to weather

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,5 +1,62 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import missingData from "../../mock/weather";
+
+import * as classes from "./Weather.module.scss";
+
+const labels = ["Home", "Weather"];
+const sliderSettingsBuilder = () => ({
+  dragging: false,
+  infinite: false,
+  autoplay: false,
+  dots: true,
+  dotsClass: `slick-dots ${classes.dots}`,
+  arrows: false,
+  className: classes.slider,
+  accessibility: true,
+  focusOnSelect: true,
+  lazyLoad: "ondemand",
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  customPaging: function (i) {
+    return (
+      <div>
+        <span>{labels[i]}</span>
+      </div>
+    );
+  },
+  // nextArrow: (
+  //   <NextArrow>
+  //     <RightArrowWide className={styles.arrows} />
+  //   </NextArrow>
+  // ),
+  // prevArrow: (
+  //   <PrevArrow>
+  //     <LeftArrowWide className={styles.arrows} />
+  //   </PrevArrow>
+  // ),
+});
+
 const Weather = () => {
-  return <div>Weather</div>;
+  return (
+    <div className={classes.Weather}>
+      <Slider {...sliderSettingsBuilder()}>
+        <section>
+          <h2>Home</h2>
+        </section>
+        <section>
+          <h2>{missingData.title}</h2>
+          <h3>{missingData.subtitle}</h3>
+          <form>
+            <input type="text" />
+            <button type="submit">{missingData.submit}</button>
+          </form>
+        </section>
+      </Slider>
+    </div>
+  );
 };
 
 export default Weather;
